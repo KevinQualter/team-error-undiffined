eb769e4695d011f20c92e5e6349b279b5d6aa12b
qualk2343 <qualk2343@FHC-IC0034105.wrdsb.ca>
2014-11-24 12:20:24 -0500

    testing animated movement

---
+++ src/KevinScratch/PanStats.java
@@ -0,0 +1,13 @@
+package KevinScratch;
+
+import version1.*;
+import java.awt.Color;
+import java.awt.Toolkit;
+import javax.swing.JPanel;
+
+public class PanStats extends JPanel {
+
+    public PanStats() {
+        setBackground(Color.BLACK);
+    }
+}
